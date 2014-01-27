More precision fixes to grid and earth_distance

lib.py
@@ -71,7 +71,7 @@ def earth_distance(lon1, lat1, lon2, lat2):
     sin_latj = tan_lat / np.sqrt(1.0+tan_lat**2)
 
     return radius*np.sqrt(2.0*(1.0-cos_lat*cos_latj* \
-                  np.cos(d2r*(lon1-lon2))-sin_lat*sin_latj)).astype(np.float32)
+                  np.cos(d2r*(lon1-lon2))-sin_lat*sin_latj))
 
 def rotate(u, v, angle):
     """

model/grid.py
@@ -154,20 +154,20 @@ class grid:
         if "pm" in self.__dict__:
             self.dm = 1.0/self.pm
         else:
-            self.dm = np.ones(self.lon_rho.shape)
+            self.dm = np.ones(self.lon_rho.shape,dtype=np.float32)
             self.dm[:,0:-1] = seapy.earth_distance( self.lon_rho[:,1:], \
-                                              self.lat_rho[:,1:], \
-                                              self.lon_rho[:,0:-1], \
-                                              self.lat_rho[:,0:-1] )
+                                      self.lat_rho[:,1:], \
+                                      self.lon_rho[:,0:-1], \
+                                      self.lat_rho[:,0:-1]).astype(np.float32)
             self.dm[:,-1] = self.dm[:,-2]
         if "pn" in self.__dict__:
             self.dn = 1.0/self.pn
         else:
-            self.dn = np.ones(self.lat_rho.shape)
+            self.dn = np.ones(self.lat_rho.shape,dtype=np.float32)
             self.dn[0:-1,:] = seapy.earth_distance( self.lon_rho[1:,:], \
-                                              self.lat_rho[1:,:], \
-                                              self.lon_rho[0:-1,:], \
-                                              self.lat_rho[0:-1,:] )
+                                      self.lat_rho[1:,:], \
+                                      self.lon_rho[0:-1,:], \
+                                      self.lat_rho[0:-1,:] ).astype(np.float32)
             self.dn[-1,:] = self.dn[-2,:]
         
         # Compute the Coriolis