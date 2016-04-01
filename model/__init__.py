f596dff898031029224cce067daacf456ca01264
Brian Powell <powellb@hawaii.edu>
2016-04-01 12:29:24 -1000

    Update seapy to use latest python libraries. Fix numerous bugs, add routine for computing water properties (model/lib) and tidal properties (tidal_energy).

--- model/__init__.py
+++ model/__init__.py
@@ -1,3 +1,29 @@
+"""
+  State Estimation and Analysis for PYthon
+
+    Module for working with oceanographic data and models
+
+  Copyright (c)2016 University of Hawaii under the BSD-License.
+
+  Import classes include:
+
+  - :class:`~seapy.model.grid`
+
+  Imported functions include:
+
+  - :func:`~seapy.model.lib.bvf`
+  - :func:`~seapy.model.lib.density`
+  - :func:`~seapy.model.hycom.load_history`
+  - :func:`~seapy.model.soda.load_history`
+  - :func:`~seapy.model.lib.pressure`
+  - :func:`~seapy.model.lib.rho2u`
+  - :func:`~seapy.model.lib.rho2v`
+  - :func:`~seapy.model.lib.sound`
+  - :func:`~seapy.model.lib.u2rho`
+  - :func:`~seapy.model.lib.v2rho`
+  - :func:`~seapy.model.lib.v2rho`
+  - :func:`~seapy.model.lib.w`
+"""
 from .grid import grid, asgrid
 from .lib import *
 from .hycom import *
